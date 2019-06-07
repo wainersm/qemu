28b5d5106a3e29dc200d6c66a934624da69874a9
Wainer dos Santos Moschetta <wainersm@redhat.com>
2019-06-07 14:36:34 -0400

    tests/acceptance: make LinuxInitrd inherit BootKernelTest
    Changed LinuxInitrd inherit BootKernelTest class and then use
    the utilities methods to fetch and boot a Linux Kernel.
    
    The test_with_2gib_file_should_work_with_linux_v4_16 requires
    just a recent kernel, so now using kernel from Fedora 29
    instead of 28.
    
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>

--- tests/acceptance/linux_initrd.py
+++ tests/acceptance/linux_initrd.py
@@ -12,10 +12,9 @@ import logging
 import tempfile
 from avocado.utils.process import run
 
-from avocado_qemu import Test
+from avocado_qemu import linux_kernel
 
-
-class LinuxInitrd(Test):
+class LinuxInitrd(linux_kernel.BootKernelTest):
     """
     Checks QEMU evaluates correctly the initrd file passed as -initrd option.
 
@@ -31,10 +30,7 @@ class LinuxInitrd(Test):
         Fedora-18 shipped with linux-3.6 which have not supported xloadflags
         cannot support more than 2GiB initrd.
         """
-        kernel_url = ('https://archives.fedoraproject.org/pub/archive/fedora/li'
-                      'nux/releases/18/Fedora/x86_64/os/images/pxeboot/vmlinuz')
-        kernel_hash = '41464f68efe42b9991250bed86c7081d2ccdbb21'
-        kernel_path = self.fetch_asset(kernel_url, asset_hash=kernel_hash)
+        self.fetch_kernel(provider_name='fedora', version='18', arch='x86_64')
         max_size = 2 * (1024 ** 3) - 1
 
         with tempfile.NamedTemporaryFile() as initrd:
@@ -42,7 +38,7 @@ class LinuxInitrd(Test):
             initrd.write(b'\0')
             initrd.flush()
             cmd = "%s -kernel %s -initrd %s -m 4096" % (
-                  self.qemu_bin, kernel_path, initrd.name)
+                  self.qemu_bin, self.kernel.path, initrd.name)
             res = run(cmd, ignore_status=True)
             self.assertEqual(res.exit_status, 1)
             expected_msg = r'.*initrd is too large.*max: \d+, need %s.*' % (
@@ -54,10 +50,6 @@ class LinuxInitrd(Test):
         QEMU has supported up to 4 GiB initrd for recent kernel
         Expect guest can reach 'Unpacking initramfs...'
         """
-        kernel_url = ('https://mirrors.kernel.org/fedora/releases/28/'
-                      'Everything/x86_64/os/images/pxeboot/vmlinuz')
-        kernel_hash = '238e083e114c48200f80d889f7e32eeb2793e02a'
-        kernel_path = self.fetch_asset(kernel_url, asset_hash=kernel_hash)
         max_size = 2 * (1024 ** 3) + 1
 
         with tempfile.NamedTemporaryFile() as initrd:
@@ -66,19 +58,7 @@ class LinuxInitrd(Test):
             initrd.flush()
 
             self.vm.set_machine('pc')
-            self.vm.set_console()
-            kernel_command_line = 'console=ttyS0'
-            self.vm.add_args('-kernel', kernel_path,
-                             '-append', kernel_command_line,
-                             '-initrd', initrd.name,
-                             '-m', '5120')
-            self.vm.launch()
-            console = self.vm.console_socket.makefile()
-            console_logger = logging.getLogger('console')
-            while True:
-                msg = console.readline()
-                console_logger.debug(msg.strip())
-                if 'Unpacking initramfs...' in msg:
-                    break
-                if 'Kernel panic - not syncing' in msg:
-                    self.fail("Kernel panic reached")
+            self.vm.add_args('-initrd', initrd.name, '-m', '5120')
+            self.boot_kernel(provider_name='fedora', version='29',
+                             arch='x86_64')
+            self.wait_for_console_pattern('Unpacking initramfs...')
